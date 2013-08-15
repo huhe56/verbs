01a2fa2694fa08bab48708302c63f98a3e2df5a9
huan.he <huan.he@gmail.com>
2013-08-15 23:55:20 +0000

    remove via head node ssh

---  verbs/usnic/src/test/test5.py
+++  verbs/usnic/src/test/test5.py
@@ -5,24 +5,27 @@ Created on Aug 8, 2013
 '''
 
 
-from lib.head_node import HeadNode
-from lib.compute_node import ComputeNode
+from lib.node_head import NodeHead
+from lib.node_compute import NodeCompute
 
 
 if __name__ == '__main__':
     
-    head_node = HeadNode("10.193.212.18", "huhe")
-    
-    head_node._ssh.send_expect_prompt("ls")
-    #head_node._ssh.send("exit")
-    
-    compute_node = ComputeNode(head_node, "node01")
-    
-    #compute_node.send_expect_prompt("ls")
-    #compute_node.send_expect_prompt("pwd")
-    compute_node.send_expect_prompt("exit\n")
-    
+    '''
+    head_node = NodeHead("10.193.212.18", "huhe")
+    head_node.send_expect_prompt("ls -l")
+    head_node.send_expect_prompt("pwd")
     head_node._ssh.send("exit")
+    '''
+    
+    compute_node = NodeCompute("node01")
+    compute_node.send_expect_prompt("ls")
+    compute_node.send_expect_prompt("pwd")
+    eth_if_list = compute_node.get_eth_list()
+    print eth_if_list
+    compute_node._ssh.send("exit")
+    
+    
     
     
     
