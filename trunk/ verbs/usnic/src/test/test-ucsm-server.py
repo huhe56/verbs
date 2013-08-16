7dac0dcfd0b5a942be08c26ce3c2d10bd3f27bc5
huan.he@gmail.com <huan.he@gmail.com@7884d1cf-3f8e-9434-ecfc-e28593f47976>
2013-08-16 01:45:09 +0000

    ucsm get cpu brief

---
+++ trunk/ verbs/usnic/src/test/test-ucsm-server.py
@@ -0,0 +1,22 @@
+'''
+Created on Aug 8, 2013
+
+@author: huhe
+'''
+
+
+from main.define import Define
+from lib.ucsm import UCSM
+from lib.ucsm_server import UcsmServer
+
+
+if __name__ == '__main__':
+    
+    ucsm_server_list = UcsmServer.init_ucsm_server(Define.UCSM_HOSTNAME)
+        
+    for ucsm_server in ucsm_server_list:
+        output = ucsm_server.get_cpu_brief()
+        
+    
+    
+    
