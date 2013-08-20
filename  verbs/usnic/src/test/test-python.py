88afba7de066d53118f97ed7d82be26d38588e95
huan.he <huan.he@gmail.com>
2013-08-20 02:07:18 +0000

    util run steps

---
+++  verbs/usnic/src/test/test-python.py
@@ -0,0 +1,29 @@
+'''
+Created on Aug 19, 2013
+
+@author: huhe
+'''
+
+import simplejson as json
+import re 
+
+
+from main.define import Define
+from lib.util import Util
+
+
+if __name__ == '__main__':
+    '''
+    str1 = "wget Define.URL_IMAGE_LIST[0] -P Define.PATH_DOWNLOAD"
+    print repr(str1)
+    #str1 = string.Template("wget ${Define.URL_IMAGE_LIST[0]}".substitute(locals()))
+    print eval("Define.PATH_DOWNLOADS")
+    #print str1
+    for i in eval("range(0, 3)"):
+        print i
+        
+    file_json_step = Define.PATH_USNIC_JSON_LINUX + "wget_ucsm_firmware.json"    
+    Util.run_test_step_list(file_json_step)
+    '''
+    
+    
---  verbs/usnic/src/test/test_ucsm_server.py
+++  verbs/usnic/src/test/test_ucsm_server.py
@@ -6,16 +6,24 @@ Created on Aug 8, 2013
 
 
 from main.define import Define
+from lib.ucsm import UCSM
 from lib.ucsm_server import UcsmServer
 
 
 if __name__ == '__main__':
     
+    
     ucsm_server_list = UcsmServer.init_ucsm_server(Define.UCSM_HOSTNAME)
-        
     for ucsm_server in ucsm_server_list:
-        output = ucsm_server.get_cpu_brief()
-        
+        #output = ucsm_server.show_cpu_brief()
+        print ucsm_server._service_profile
+    
+    '''
+    ucsm = UCSM(Define.UCSM_HOSTNAME);
+    print "\n--------\n" + ucsm.show_cup_brief()
+    '''
+    
+    
     
     
     
