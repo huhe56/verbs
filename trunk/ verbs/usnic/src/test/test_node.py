38343bf0c561bde1857a428af4d7dd7176c0a0d0
huan.he@gmail.com <huan.he@gmail.com@7884d1cf-3f8e-9434-ecfc-e28593f47976>
2013-08-20 02:07:18 +0000

    util run steps

--- trunk/ verbs/usnic/src/test/test_node.py
+++ trunk/ verbs/usnic/src/test/test_node.py
@@ -4,25 +4,30 @@ Created on Aug 8, 2013
 @author: huhe
 '''
 
-
+from main.define import Define
+from lib.util import Util
 from lib.node_compute import NodeCompute
+from lib.node_head import NodeHead
 
 
 if __name__ == '__main__':
     
-    '''
     head_node = NodeHead("10.193.212.18", "huhe")
-    head_node.send_expect_prompt("ls -l")
-    head_node.send_expect_prompt("pwd")
-    head_node._ssh.send("exit")
+    '''
+    file_json_step = Define.PATH_USNIC_JSON_LINUX + "wget_ucsm_firmware.json"   
+    Util.run_step_list(head_node.get_ssh(), file_json_step)
     '''
     
+    
+    
+    '''
     compute_node = NodeCompute("node01")
     compute_node.send_expect_prompt("ls")
     compute_node.send_expect_prompt("pwd")
     eth_if_list = compute_node.get_eth_list()
     print eth_if_list
     compute_node._ssh.send("exit")
+    '''
     
     
     
