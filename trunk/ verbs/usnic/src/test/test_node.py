b20efd0a450db57ddfef7acfaeef9821c060ed61
huan.he@gmail.com <huan.he@gmail.com@7884d1cf-3f8e-9434-ecfc-e28593f47976>
2013-08-21 15:42:39 +0000

    minor reorg

--- trunk/ verbs/usnic/src/test/test_node.py
+++ trunk/ verbs/usnic/src/test/test_node.py
@@ -14,19 +14,19 @@ if __name__ == '__main__':
     
     head_node = NodeHead("10.193.212.18", "huhe")
     
-    '''
+    
     file_json_step = Define.PATH_USNIC_JSON_LINUX + "wget_ucsm_firmware.json"   
     Util.run_step_list(head_node.get_ssh(), file_json_step)
+    
+    
     '''
-    
-    
     compute_node = NodeCompute("node01")
     compute_node.send_expect_prompt("ls")
     compute_node.send_expect_prompt("pwd")
     eth_if_list = compute_node.get_eth_if_list()
     print eth_if_list
     compute_node._ssh.send("exit")
-    
+    '''
     
     
     
--- trunk/ verbs/usnic/src/test/test_ucsm_server.py
+++ trunk/ verbs/usnic/src/test/test_ucsm_server.py
@@ -15,12 +15,12 @@ if __name__ == '__main__':
     
     ucsm_server_list = UcsmServer.init_ucsm_server(Define.UCSM_HOSTNAME)
     for ucsm_server in ucsm_server_list:
-        #output = ucsm_server.show_cpu_brief()
+        output = ucsm_server.show_cpu_brief()
         print ucsm_server._service_profile
     
     
     ucsm = UCSM(Define.UCSM_HOSTNAME);
-    print "\n--------\n" + ucsm.show_cup_brief()
+    print "\n--------\n" + ucsm.show_cpu_brief()
     
     
     
