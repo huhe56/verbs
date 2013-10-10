4ecbd9c11e9dd8b29656e2a9cb437f4708cd773e
huan.he <huan.he@gmail.com>
2013-10-10 16:24:16 +0000

    associate and disassociate

---
+++  verbs/usnic/src/cmd/ucsm/disassociate-service-profile.py
@@ -0,0 +1,19 @@
+'''
+Created on Aug 21, 2013
+
+@author: huhe
+'''
+
+from main.define import Define
+from lib.ucsm_server import UcsmServer
+
+
+if __name__ == '__main__':
+        
+    ucsm_server_list = UcsmServer.init_ucsm_server(Define.UCSM_HOSTNAME)
+    for ucsm_server in ucsm_server_list:
+        
+            ucsm_server.scope_service_profile_from_top()
+            ucsm_server.disassociate_service_profile()
+
+    
